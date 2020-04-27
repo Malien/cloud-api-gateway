8b4c5a030d980a18a0ff5807f2ed6300e7915062
Yaroslav <q.link0.p@gmail.com>
2020-04-28 00:00:15 +0300

    simplified logic a bit

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
 import express, { Request, Response, NextFunction } from "express"
 import { version, author, name } from "./package.json"
-import httpProxy from "express-http-proxy";
+import proxy from "express-http-proxy";
 
 const { BIND_ADDRESS, PORT, AUTH_SERVICE_URL } = process.env
 
 const app = express()
 
-const forward = (url: string) => {
-    const proxy = httpProxy(url)
-    return (req: Request, res: Response, next: NextFunction) => proxy(req, res, next)
-}
-
-app.all("/auth", forward(AUTH_SERVICE_URL!))
+app.use("/auth", proxy(AUTH_SERVICE_URL!))
 app.get("/serviceinfo", (req, res) => {
     res.end(JSON.stringify({ ok: true, version, author, name }))
 })
